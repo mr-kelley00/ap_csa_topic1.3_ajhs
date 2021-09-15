13138ebea8945d0776e8e979fc520b64c3ba4977
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-15 09:27:36 -0400

    v0.5
    Declared int and double values.

--- ExpressionsAssignmentStatements.java
+++ ExpressionsAssignmentStatements.java
@@ -1,12 +1,14 @@
-// Author Name, Date, Time, AP CSA Topic 1.3, version 0.0
+// Author Name, Date, Time, AP CSA Topic 1.3, version 0.5
 
 public class ExpressionsAssignmentStatements {
 
     public static void main(String[] args) {
         // Declare an int variable named myInt0 and myInt1, assign them any value.
-
-        // Declare a int variable named myDouble0 and myDouble1, assign them any value.
-
+        int myInt0 = 5; 
+        int myInt1 = -17;
+        // Declare a double variable named myDouble0 and myDouble1, assign them any value.
+        double myDouble0 = 1.32;
+        double myDouble1 = -14.25; 
         /*
          * Write code that uses all five arithmetic operators on two int values, two
          * double values, and one int and one double value. Print those values on
