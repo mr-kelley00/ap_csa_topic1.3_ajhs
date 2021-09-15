dc4432c7b5e31f07bc23f775b17a8df99d9b9b74
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-15 00:24:23 -0400

    Create ExpressionsAssignmentStatements.java
    Template version 0.0 completed.

---
+++ ExpressionsAssignmentStatements.java
@@ -0,0 +1,42 @@
+// Author Name, Date, Time, AP CSA Topic 1.3, version 0.0
+
+public class ExpressionsAssignmentStatements {
+
+    public static void main(String[] args) {
+        // Declare an int variable named myInt0 and myInt1, assign them any value.
+
+        // Declare a int variable named myDouble0 and myDouble1, assign them any value.
+
+        /*
+         * Write code that uses all five arithmetic operators on two int values, two
+         * double values, and one int and one double value. Print those values on
+         * individual lines. One example is provided for you.
+         */
+
+        // Addition
+        System.out.println(myInt0 + myInt1);
+
+        // Subtraction
+        System.out.println(myInt0 - myInt1);
+
+        // Division -- Avoid Divide by Zero!
+        System.out.println(myInt0 / myInt1);
+
+        // Write a line of code demonstrating that Java rounds down when dividing
+        // integers. Print that value on the screen.
+
+        // Multiplication
+        System.out.println(myInt0 * myInt1);
+
+        // Modulus
+        System.out.println(myInt0 % myInt1);
+
+        /*
+         * Write a COMPOUND EXPRESSION that uses at least three unique arithmetic
+         * operators and one set of parenthesis. Print the value of that expression on
+         * the screen.
+         */
+
+    }
+
+}
